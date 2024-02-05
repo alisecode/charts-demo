74783662cdee10c2f4cbc684a6f3a12b3b2163e3
Alisa <alisa.melissa@icloud.com>
2024-02-05 14:15:24 +0200

    Initial Commit

---
+++ WalletManager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WalletManager
+//
+//  Created by Алиса Третьякова on 05.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WalletManager/WalletManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalletManagerApp.swift
+//  WalletManager
+//
+//  Created by Алиса Третьякова on 05.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct WalletManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
